23177c5be6653b3369c9921a51c180466e9c64a9
alimousavi007 <22558086+alimousavi007@users.noreply.github.com>
2025-09-14 01:16:14 +0330

    Feat: Implement dark mode toggle feature

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-const colorButton = document.querySelector("#change-color-btn");
 const body = document.querySelector("body");
-function changeBackgroundColor() {
-  body.style.backgroundColor = '#e0e0e0';
-  console.log('رنگ پس‌زمینه تغییر کرد!');
-}
-colorButton.addEventListener('click', changeBackgroundColor);
+const darkModeButton = document.querySelector("#dark-mode-toggle");
+darkModeButton.addEventListener("click", (e) =>{
+    e.preventDefault();
+    body.classList.toggle("dark-mode");
+});
